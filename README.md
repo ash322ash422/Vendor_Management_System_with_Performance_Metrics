6212ee089bb261348de1c984a822591873208013
ash322ash422 <160450942+ash322ash422@users.noreply.github.com>
2024-05-01 22:39:24 +0530

    summary

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Vendor_Management_System_with_Performance_Metrics
  
+ I used python3.11 for this project.
---
+++ VMS/vendors/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+CHOICES_STATUS= (('pending','Pending'),
+                 ('completed','Completed'),
+                 ('cancelled','cancelled')
+                 )
+
+# Create your models here.
+class Vendor(models.Model):
+    name =  models.CharField(max_length=50, blank=True, null=True) # Vendor's name.
+    contact_details = models.TextField(help_text="Contact information of the vendor") # Contact information of the vendor.
+    address = models.TextField(help_text="Physical address of the vendor") #Physical address of the vendor.
+    vendor_code = models.CharField(unique=True, max_length=10) # A unique identifier for the vendor.
+    on_time_delivery_rate = models.FloatField(help_text="Tracks the percentage of on-time deliveries") # Tracks the percentage of on-time deliveries.
+    quality_rating_avg = models.FloatField(help_text="Average rating of quality based on purchase orders") # Average rating of quality based on purchase orders.
+    average_response_time = models.FloatField(help_text="Average time taken to acknowledge purchase orders") # Average time taken to acknowledge purchase orders.
+    fulfillment_rate = models.FloatField(help_text="Percentage of purchase orders fulfilled successfully") # Percentage of purchase orders fulfilled successfully
+#end class
+
+class PurchaseOrder(models.Model):
+    po_number = models.CharField(unique=True) # Unique number identifying the PO.
+    vendor = models.ForeignKey(to=Vendor, on_delete=models.SET_NULL) # Link to the Vendor model.
+    order_date = models.DateTimeField(auto_now_add=True) # Date when the order was placed.
+    delivery_date = models.DateTimeField(blank=True) # Expected or actual delivery date of the order.
+    items = models.JSONField() # Details of items ordered.
+    quantity = models.IntegerField(default=1) # Total quantity of items in the PO.
+    status = models.CharField(choices=CHOICES_STATUS, blank=False) # Current status of the PO (e.g., pending, completed, canceled).
+    quality_rating = models.FloatField(blank=True) # Rating given to the vendor for this PO (nullable).
+    issue_date = models.DateTimeField(blank=False) # Timestamp when the PO was issued to the vendor.
+    acknowledgment_date = models.DateTimeField(blank=True) #, nullable - Timestamp when the vendor acknowledged the PO
+#end class
+    
+class HistoricalPerformance(models.Model):
+    vendor = models.ForeignKey(to=Vendor, on_delete=models.CASCADE) # - Link to the Vendor model.
+    date = models.DateTimeField(blank=False) # Date of the performance record.
+    on_time_delivery_rate = models.FloatField() # Historical record of the on-time delivery rate.
+    quality_rating_avg = models.FloatField() # Historical record of the quality rating average.
+    average_response_time = models.FloatField() # Historical record of the average response time.
+    fulfillment_rate = models.FloatField() # Historical record of the fulfillment rate
+#end class
+
+
+
